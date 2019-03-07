7f767b8eab053ee663141100c1a4dc5163d3438f
Kang <HKang@calacademy.org>
2019-03-07 15:00:36 -0800

    entry is working, produces 2 different kinds of buttons

--- index.js
+++ index.js
@@ -12,15 +12,14 @@ myFunction= ()=>{
   for (var i=0; i<text_as_array.length; i++) {
 	let line = text_as_array[i];
   	let split_line = line.split(',');
-	console.log(split_line);
-    if (split_line.length==2){
+	if (split_line.length==2){
     	result.push(create_message(split_line))
     }
     if (split_line.length==3){
     	result.push(create_toggle(split_line))
     }
-	}
-  let finalResult = result.join()
+  }
+  let finalResult = result.join("")
   buttonResult.value=finalResult;
   
 }
@@ -29,7 +28,7 @@ function create_message(commands) {
 	console.log("creates a message button");
   let command = commands[0];
   let button_text=commands[1];
-  let result = `<button class=\"uv-message-button\" data-message=\"${command}\">${button_text}</button>\n`;
+  let result = `<div class=\"ui-block-b\">\n\t<button class=\"uv-message-button\" data-message=\"${command}\">${button_text}</button>\n</div>\n`;
   console.log(result);
   return result;
 }
@@ -38,7 +37,7 @@ function create_toggle(commands) {
 	let command1 = commands[0];
   let command2=commands[1];
   let button_text=commands[2];
-  let result = `<button class=\"uv-toggle-off\" data-message-on=\"${command1}\" data-message-off=\"${command2}\">${button_text}</button>\n`;
+  let result = `<div class=\"ui-block-b\">\n\t<button class=\"uv-toggle-off\" data-message-on=\"${command1}\" data-message-off=\"${command2}\">${button_text}</button>\n</div>\n`;
   console.log(result);
 
   return result;
